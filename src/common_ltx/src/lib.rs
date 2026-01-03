f1113fef975543378e1f21d6b7dc3b6c78b3f953
Malcolm Greaves <malcolmgreaves@users.noreply.github.com>
2026-01-02 16:47:36 -0800

    Simple openai prompt-response integration w/ unittest (#3)

--- src/common_ltx/src/lib.rs
+++ src/common_ltx/src/lib.rs
@@ -1,1 +1,7 @@
-
+/// True if the environment variable is set and not empty. False otherwise.
+pub fn is_env_set(env_var: &str) -> bool {
+    match std::env::var(env_var) {
+        Ok(val) => !val.is_empty(),
+        Err(_) => false,
+    }
+}
