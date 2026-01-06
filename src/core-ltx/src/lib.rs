5f8e7c879d9216c55d841791a2c0b0cf28ca2112
Malcolm Greaves <75upbeats_hybrids@icloud.com>
2026-01-06 10:36:23 -0800

    HTML parsing & validation

--- src/core-ltx/src/lib.rs
+++ src/core-ltx/src/lib.rs
@@ -3,41 +3,65 @@ pub mod md_llm_txt;
 pub mod web_html;
 
 pub use md_llm_txt::{LlmsTxt, Markdown, is_valid_markdown, validate_is_llm_txt};
-pub use web_html::{download_html, is_valid_url};
+pub use web_html::{download, is_valid_url, parse_html};
 
+/// Custom error type for website downloading & llms.txt format validation.
 #[derive(Debug)]
 pub enum Error {
-    InvalidMarkdown(nom::Err<nom::error::Error<String>>),
-    InvalidLlmsTxtFormat(String),
-    DownloadError(reqwest::Error),
+    /// Cannot download website because user supplied an invalid URL.
     InvalidUrl(url::ParseError),
-    InaccessibleWebsite(String),
+
+    /// Website download failed.
+    DownloadError(reqwest::Error),
+
+    /// HTML is invalid, even after attempting to fix using HTML5 rules.
     InvalidHtml(String),
+
+    /// File is not valid markdown.
+    InvalidMarkdown(nom::Err<nom::error::Error<String>>),
+
+    /// Markdown file does not adhere to the llms.txt format.
+    InvalidLlmsTxtFormat(String),
 }
 
 impl std::fmt::Display for Error {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
+            Error::InvalidUrl(url) => write!(f, "Not a valid URL: {}", url),
+            Error::DownloadError(err) => write!(f, "Download error: {}", err),
+            Error::InvalidHtml(txt) => write!(f, "Not a valid HTML: {}", txt),
             Error::InvalidMarkdown(err) => write!(f, "Not valid Markdown: {}", err),
             Error::InvalidLlmsTxtFormat(msg) => write!(f, "Not valid llms.txt Format: {}", msg),
-            Error::InvalidUrl(url) => write!(f, "Not a valid URL: {}", url),
-            Error::InaccessibleWebsite(url) => write!(f, "Cannot reach website: {}", url),
-            Error::InvalidHtml(txt) => write!(f, "Not a valid HTML: {}", txt),
-            Error::DownloadError(err) => write!(f, "Download error: {}", err),
         }
     }
 }
 
 impl std::error::Error for Error {}
 
+/// Request errors occur during the download process.
 impl From<reqwest::Error> for Error {
     fn from(err: reqwest::Error) -> Self {
         Error::DownloadError(err)
     }
 }
 
+/// URL parsing errors occur during the URL validation process.
 impl From<url::ParseError> for Error {
     fn from(err: url::ParseError) -> Self {
         Error::InvalidUrl(err)
+    }
+}
+
+/// Converting from bytes to UTF-8 strings occurs in the HTML parsing & validation process.
+impl From<std::string::FromUtf8Error> for Error {
+    fn from(err: std::string::FromUtf8Error) -> Self {
+        Error::InvalidHtml(err.to_string())
+    }
+}
+
+/// io Errors occur during the HTML parsing & validation process.
+impl From<std::io::Error> for Error {
+    fn from(err: std::io::Error) -> Self {
+        Error::InvalidHtml(err.to_string())
     }
 }
--- src/core-ltx/src/web_html.rs
+++ src/core-ltx/src/web_html.rs
@@ -1,13 +1,42 @@
-use crate::Error;
 use url::Url;
 
+use html5ever::{
+    parse_document,
+    serialize::{SerializeOpts, serialize},
+    tendril::TendrilSink,
+};
+use markup5ever_rcdom::{RcDom, SerializableHandle};
+
+use crate::Error;
+
+/// Validates that the input string is a URL.
 pub fn is_valid_url(url: &str) -> Result<Url, Error> {
     let valid_url = Url::parse(url)?;
     Ok(valid_url)
 }
 
-pub async fn download_html(url: &Url) -> Result<String, Error> {
+/// Downloads the website's content as text.
+pub async fn download(url: &Url) -> Result<String, Error> {
     let response = reqwest::get(url.as_str()).await?;
     let text_body = response.text().await?;
     Ok(text_body)
+}
+
+/// Parses and validates the input as HTML. Returns valid HTML 5 or an error.
+/// Attempts to fix the input string according to HTML5 parsing rules.
+pub fn parse_html(content: &str) -> Result<String, Error> {
+    let dom: RcDom = parse_document(RcDom::default(), Default::default())
+        .from_utf8()
+        .read_from(&mut content.as_bytes())?;
+
+    let document: SerializableHandle = dom.document.clone().into();
+
+    let output = {
+        let mut output: Vec<u8> = Vec::new();
+        serialize(&mut output, &document, SerializeOpts::default())?;
+        output
+    };
+
+    let html = String::from_utf8(output)?;
+    Ok(html)
 }
